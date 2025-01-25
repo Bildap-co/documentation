0c08c4e7417654a4f9728e99ae739c07b55296c8
Mahan <>
2025-01-25 18:24:31 +0100

    deploy on docs.bildap.co

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://bildap-co.github.io',
+  url: 'https://docs.bildap.co',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
